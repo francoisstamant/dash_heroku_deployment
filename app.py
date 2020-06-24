03a663eb23bed892cc6dac335e1c4794830f1f8b
François St-Amant <56452799+francoisstamant@users.noreply.github.com>
2020-06-24 11:31:50 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,30 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+server = app.server
+
+top_markdown_text = '''
+### Dash Boilerplate
+This is my first deployed app
+'''
+
+app.layout = html.Div([
+
+    dcc.Markdown(children=top_markdown_text),
+
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
+
+
+
+
+
